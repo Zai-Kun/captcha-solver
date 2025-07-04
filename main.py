257d051f06d40ee96e3d4e53012ac677b6fa13c1
Zai-Kun <122824602+Zai-Kun@users.noreply.github.com>
2025-07-04 13:32:15 +0500

    initial upload

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ shadow_offset = (2, 2)
 line_width = 3
 line_count = 3
 letters_per_image = 3
-images_per_font = 200
+images_per_font = 800
 
 characters = string.ascii_letters + string.digits
 valid_classes = list(string.ascii_lowercase + string.digits)
