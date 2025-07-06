b8f9feb668583a0793a8c3e9d110aee3895a5f16
Zai-Kun <122824602+Zai-Kun@users.noreply.github.com>
2025-07-06 17:42:10 +0500

    initial upload

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ outline_width = 2
 line_width = 3
 line_count = 7
 letters_per_image = 3
-images_per_font = 1000
+images_per_font = 6000
 
 characters = string.ascii_letters + string.digits
 valid_classes = list(string.ascii_lowercase + string.digits)
